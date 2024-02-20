4c15fe7f6ebb451f218a2cf7eaf2826dfc7ab41b
jacklion <104385151+jacklion710@users.noreply.github.com>
2024-02-20 16:34:22 -0500

    Drag and drop event listener

---
+++ static/js/script.js
@@ -0,0 +1,46 @@
+// static/js/script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    let dropArea = document.getElementById('drop-area');
+
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
+    });
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false);
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false);
+    });
+
+    function highlight(e) {
+        dropArea.classList.add('highlight');
+    }
+
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight');
+    }
+
+    dropArea.addEventListener('drop', handleDrop, false);
+
+    function handleDrop(e) {
+        let dt = e.dataTransfer;
+        let files = dt.files;
+
+        handleFiles(files);
+    }
+
+    function handleFiles(files) {
+        let fileInput = document.getElementById('fileElem');
+        fileInput.files = files;
+        let event = new Event('change');
+        fileInput.dispatchEvent(event);
+    }
+});
